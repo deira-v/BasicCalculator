ea5512b9c0c1208b535ef3e13da00491567e1468
Deira Valci <deiravalci@gmail.com>
2025-09-26 08:10:33 +0100

    Taking in first number

--- src/main/java/ie/atu/BasicCalculator.java
+++ src/main/java/ie/atu/BasicCalculator.java
@@ -1,7 +1,16 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
     public static void main(String[] args) {
+        //Prompt a number
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please Enter first number: ");
+
+        double firstNumber = scan1.nextDouble();
+        System.out.println(firstNumber);
+
 
     }
 }
