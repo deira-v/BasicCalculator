bdb03a26d653e7782f5421da457625f154649508
Deira Valci <deiravalci@gmail.com>
2025-09-26 08:23:52 +0100

    first addition

--- src/main/java/ie/atu/BasicCalculator.java
+++ src/main/java/ie/atu/BasicCalculator.java
@@ -8,9 +8,16 @@ public class BasicCalculator {
         Scanner scan1 = new Scanner(System.in);
         System.out.println("Please Enter first number: ");
 
+        //Enter first number
         double firstNumber = scan1.nextDouble();
         System.out.println(firstNumber);
 
+        //Enter second number
+        System.out.println("Please Enter second number: ");
+        double secondNumber = scan1.nextDouble();
+        System.out.println("You entered:" + secondNumber);
 
+        double sum = firstNumber + secondNumber;
+        System.out.println("The sum is:" +sum);
     }
 }
