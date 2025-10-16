f37f84bcf3de881a6d896c3e5a99cf5eff294dde
deira <deiravalci@gmail.com>
2025-10-16 21:33:56 +0100

    switch statement

--- src/main/java/ie/atu/BasicCalculator.java
+++ src/main/java/ie/atu/BasicCalculator.java
@@ -2,8 +2,10 @@ package ie.atu;
 
 import java.util.Scanner;
 
-public class BasicCalculator {
-    public static void main(String[] args) {
+public class BasicCalculator
+{
+    public static void main(String[] args)
+    {
         //Prompt a number
         Scanner scan1 = new Scanner(System.in);
         System.out.println("Please Enter first number: ");
@@ -17,25 +19,47 @@ public class BasicCalculator {
         double secondNumber = scan1.nextDouble();
         System.out.println("You entered:" + secondNumber);
 
-        double sum = firstNumber + secondNumber;
-        System.out.println("The sum is:" +sum);
+        //switch statement
+        System.out.println("Please choose an operation:\n Add; Sub; Mult, Dev");
+        String operation = scan1.next();
 
-        //addition and subtraction operations
-        int operation = 0;
-        double result = switch (operation) {
-            case "add" -> {
+
+        double result = switch(operation)
+        {
+
+            case "add" ->
+            {
                 yield firstNumber + secondNumber;
             }
-
-            case "subtract" -> {
+            case "sub" ->
+            {
                 yield firstNumber - secondNumber;
             }
+            case "mult" ->
+            {
+                yield firstNumber * secondNumber;
+            }
+            case "dev" ->
+            {
+                if(secondNumber != 0)
+                {
+                    yield firstNumber / secondNumber;
 
-            //multiple and divide options
+                }else
+                {
+                    System.out.println("You can't divide by zero");
+                    yield 0;
+                }
+            }
+            default ->
+            {
+                System.out.println("Wrong Entry.");
+                yield 0;
+            }
 
-        }
 
-
+        };
+        System.out.println("the Result is: " + result);
 
     }
 }
